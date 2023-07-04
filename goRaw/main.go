36e0679f7d63f8fae08f8b16f26e29ad3b8fe019
bowen-intel <bowen.song@intel.com>
2023-07-04 16:51:55 +0800

    feat: get cgroup from k8s

--- goRaw/main.go
+++ goRaw/main.go
@@ -1,7 +1,9 @@
 package main
 
+import "github.com/Rouzip/goperf/utils"
+
 // run collector for 10 containers
 
 func main() {
-
+	utils.GetTestCgroupPath("inspur-icx-1")
 }
--- utils/group.go
+++ utils/group.go
@@ -1,24 +1,47 @@
 package utils
 
 import (
-	"io/fs"
-	"path/filepath"
+	"context"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
 )
 
-func GetTestCgroupPath() ([]string, error) {
-	dirs := []string{}
+const config = "/home/ubuntu/.kube/config"
 
-	path := "/sys/fs/cgroup/kube"
-	err := filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
-		if info.IsDir() {
-			dirs = append(dirs, path)
-			return nil
-		}
-		return nil
-	})
+// get test nginx container cgroup path
+func GetTestCgroupPath(node string) ([]string, error) {
+	k8sconfig, err := clientcmd.BuildConfigFromFlags("", config)
 	if err != nil {
-		return nil, err
+		klog.Fatal(err)
+	}
+	k8sClient, err := kubernetes.NewForConfig(k8sconfig)
+	if err != nil {
+		klog.Fatal(err)
 	}
 
-	return dirs, nil
+	pods, err := k8sClient.CoreV1().Pods("default").List(context.Background(), metav1.ListOptions{
+		FieldSelector: "spec.nodeName=" + node,
+	})
+
+	for _, pod := range pods.Items {
+		klog.Info(pod.Name)
+	}
+
+	return nil, nil
+	// path := "/sys/fs/cgroup/kubepods.slice"
+	// err = filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
+	// 	if info.IsDir() && strings.Contains(path, "_") {
+	// 		dirs = append(dirs, path)
+	// 		return nil
+	// 	}
+	// 	return nil
+	// })
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// return dirs, nil
 }
